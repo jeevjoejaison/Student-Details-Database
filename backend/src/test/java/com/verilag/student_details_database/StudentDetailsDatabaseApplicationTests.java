76ab432984065e7250b367cb88ff30e2454c1650
Chriss Charls <chrisscharls9@gmail.com>
2025-03-09 17:27:18 +0530

    initialized backend

---
+++ backend/src/test/java/com/verilag/student_details_database/StudentDetailsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verilag.student_details_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
